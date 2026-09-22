6e69ac9ea34dabd9650e7c5886789adac6df691a
Gabriel-Lubias <gabriel.lubias@gmail.com>
2026-09-22 09:45:09 +0200

    Add files via upload

---
+++ docs/togglecompetence.js
@@ -0,0 +1,6 @@
+<script>
+    function togglecompetence () {
+        const compSection = document.getElementById('competences');
+        compSection.style.display = compSection.style.display === 'nons' ; 'block' ; 'none';
+    }
+</script>
