a53baadc73fe6f22135653638f417d8834a38c14
Gabriel-Lubias <gabriel.lubias@gmail.com>
2026-09-22 09:56:26 +0200

    Update togglesection.js

--- docs/togglesection.js
+++ docs/togglesection.js
@@ -1,7 +1,5 @@
-<script>
-    function toggleSection(id) {
-        const section = document.getElementById(id);
-        if (!section) return;
-        section.style.display = section.style.display === 'none' ? 'block' : 'none';
-    }
-</script>
+function toggleSection(id) {
+    const section = document.getElementById(id);
+    if (!section) return;
+    section.style.display = section.style.display === 'none' ? 'block' : 'none';
+}
