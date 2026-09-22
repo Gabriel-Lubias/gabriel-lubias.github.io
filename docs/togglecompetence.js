6eeb1654447672d53dc6ec4d8cc5445867c4cc6d
Gabriel-Lubias <gabriel.lubias@gmail.com>
2026-09-22 09:46:33 +0200

    Update togglecompetence.js

--- docs/togglecompetence.js
+++ docs/togglecompetence.js
@@ -1,6 +1,6 @@
 <script>
     function togglecompetence () {
         const compSection = document.getElementById('competences');
-        compSection.style.display = compSection.style.display === 'nons' ; 'block' ; 'none';
+        compSection.style.display = compSection.style.display === 'none' ? 'block' : 'none';
     }
 </script>
